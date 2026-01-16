73566f42e66a5ca171c2691070c8255c65d488ec
invoker <codewhispererisalive@gmail.com>
2026-01-16 16:08:05 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -5,10 +5,35 @@ import (
 	"bufio"
 	"os"
 	"strings"
+	"path/filepath"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Print
+
+func isExecutable(path string) bool {
+	info, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+	return info.Mode()&0111 != 0 // file has execute permission 
+}
+
+func findInPath(cmdName string) string {
+	pathEnv := os.Getenv("PATH")
+	if pathEnv == "" {
+		return ""
+	}
+
+	paths := strings.Split(pathEnv, string(os.PathListSeparator))
+	for _, dir := range paths {
+		fullPath := filepath.Join(dir, cmdName)
+		if isExecutable(fullPath) {
+			return fullPath
+		}
+	}
+	return ""
+}
 
 func main() {
 	for {
@@ -43,7 +68,13 @@ func main() {
 			if cmdName == "echo" || cmdName == "exit" || cmdName == "type" {
 				fmt.Printf("%s is a shell builtin\n", cmdName)
 			} else {
-				fmt.Printf("%s: not found\n", cmdName)
+				// search in PATH
+				execPath := findInPath(cmdName)
+				if execPath != "" {
+					fmt.Printf("%s is %s\n", cmdName, execPath)
+				} else {
+					fmt.Printf("%s: not found\n", cmdName)
+				}	
 			}
 			continue
 		}
